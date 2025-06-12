24f4db24021b81c62638be29058558ec47fc98d8
pacse <68709871+pacse@users.noreply.github.com>
2025-06-13 08:17:31 +1200

    Update utils.py
    Added helper functions from mockup.py and improved docstrings

--- CS50xFP/utils.py
+++ CS50xFP/utils.py
@@ -2,7 +2,11 @@ from cs50 import SQL
 from dataclasses import dataclass
 
 @dataclass
-class User: # class to store user information after login
+class User:
+    '''
+    A dataclass to store important information after
+    getting a user's data from the deepwell database
+    '''
     id: int
     name: str
     password: str
@@ -12,6 +16,10 @@ class User: # class to store user information after login
     phrase: str | None = None # not required for lower level personnel
 
 def init_usr(info: dict) -> User:
+    '''
+    Stores user data from a deepewell
+    request in a user dataclass
+    '''
     id = info["id"]
     name = info["name"]
     password = info["password"]
@@ -24,3 +32,28 @@ def init_usr(info: dict) -> User:
 
 # SQL database
 db = SQL("sqlite:///SCiPNETdeepwell.db")
+
+def clear() -> None:
+    '''
+    Clears the screen
+    '''
+  if name == "nt":
+    system("cls")
+  elif use_colab:
+    output.clear() # type: ignore
+  else:
+    system("clear")
+
+# print(f"{'':^{SIZE}}")
+def printc(string: str) -> None:
+  '''
+  prints a line {string} centered to the terminal size
+  '''
+  print(f"{string:^{SIZE}}")
+
+def timestamp() -> str:
+  '''
+  gets the current timestamp
+  format: Day/Month/Year - Hour/min/second
+  '''
+  return dt.now().strftime("%d/%m/%Y - %H:%M:%S")
