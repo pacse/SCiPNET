107119f68c89533092aae53093e7ffc6aaa068f4
pacse <68709871+pacse@users.noreply.github.com>
2025-06-14 10:27:49 +1200

    Update server.py
    Fixing some type hinting, little syntax errors. Going to refactor later

--- CS50xFP/server.py
+++ CS50xFP/server.py
@@ -4,20 +4,19 @@ Server for CS50x Final Project - SCiPNET
 
 import json
 import socket
-from threading import activeCount, Thread
-from utils import init_usr, db
+from threading import active_count, Thread
+from typing import Any
+from utils import db, init_usr, User
+
+
 
 # Information for server connection
 HOST = "127.0.0.1" # localhost
 PORT = 65432
 ADDR = (HOST, PORT) # combine into a tuple
 
-# TODO: Do I want this?
-‫QUERIES‬ = { # dict to store all used sql queries, may remove
-	"auth":"SELECT * FROM users WHERE id = ? AND password = ?"
-}
 
-def encode(data: any) -> any:
+def encode(data: Any) -> Any:
     # TODO: Validate
     '''
     Encodes data into json and converty it to bytes
@@ -25,7 +24,8 @@ def encode(data: any) -> any:
     '''
     return json.dumps(data).encode()
 
-def decode(data: any) -> any:
+
+def decode(data: Any) -> Any:
     # TODO: Validate
     '''
     decodes data received from a socket connection
@@ -33,25 +33,23 @@ def decode(data: any) -> any:
     '''
     return json.loads(data.decode()).split()
 
-def auth_usr(id: int, password: str):
+
+def auth_usr(id: int, password: str) -> tuple[bool, User | None]:
     # TODO: Validate
     '''
     authenticates a user by querying the deepwell
     '''
-    print(f"Authenticating user {data[1]!r} with password: {data[2]!r}")
-    
+    print(f"Authenticating user {id!r} with password: {password!r}")
+
     # authenticate user
-    row = db.execute(QUERIES["auth"], data[1], data[2])[0] # get the dict from row 0, all ids are unique
+    row = db.execute("SELECT * FROM users WHERE id = ? AND password = ?", id, password)[0] # get the dict from row 0, all ids are unique
     if row: # sucess
-    	print("Sucess")
-    	usr = init_usr(row)
-        print(usr)
-        conn.sendall(encode(True))
+        return True, init_usr(row) # return True and a User dataclass
     else: # failure
-    	print("Failure")
-        conn.sendall(encode(False))
+    	return False, None # return False and None for User dataclass
         
-def get_auth(c: socket.socket, limit: int = 3) -> (bool, int, str):
+
+def get_auth(c: socket.socket, limit: int = 3) -> tuple[bool, int, str]:
     # TODO: Validate
     # TODO: Transmit user data securely !!!!!!!!
     '''
@@ -60,8 +58,8 @@ def get_auth(c: socket.socket, limit: int = 3) -> (bool, int, str):
     returns id and password received, bool is if limit was exceeded
     '''
     for i in range(limit): # limit client auth requests
-		c.sendall(encode("AUTH")) # request auth from client
-		data = c.recv(1024) # receive auth
+        c.sendall(encode("AUTH")) # request auth from client
+        data = c.recv(1024) # receive auth
         if data: # no more loop if we received auth
             data = decode(data) # decode data
             return False, int(data[0]), data[1] # False, id, pw
@@ -95,25 +93,25 @@ def handle_usr(client, addr):
     
     # normal server-client back and forth
     while True:
-        data = conn.recv(1024) # receive data
+        data = client.recv(1024) # receive data
         # check for no data
         if not data:
             print("Connection terminated")
             break
                         
-        data = decode() # decode data
+        data = decode(data) # decode data
         print(f"Data received: {data}")
 
-		''' 
+        ''' 
         TODO: what did the user want to do:
             * ACCESS file_ref
-            	* SCP id/name | eg. 001 or SD Locke’s Proposal would be valid. 
-                	NOTE: just accessing 001 would result in a random proposal, 
+                * SCP id/name | eg. 001 or SD Locke's Proposal would be valid. 
+                    NOTE: just accessing 001 would result in a random proposal, 
                     but if proposal is specified then use specified proposal
                 * USER id/name | eg. 0 to access info for user with id 0, or full name to access that user
                     * Must have clearance equal to or above user
                 * SITE id
-                    * Accesses site directory, shows all staff, SCPs, MTFs, Director, site mission, other .txt’s
+                    * Accesses site directory, shows all staff, SCPs, MTFs, Director, site mission, other .txt's
                 * MTF id/name
                     * Generally not classified, but missions would be classified to scp clearance
         '''
@@ -122,22 +120,21 @@ def handle_usr(client, addr):
 def main():
     # TODO: Validate
     '''
-    handles the mogic for the main thread:
+    handles the logic for the main thread:
     set up listening socket
     upon receiving a connection:
-    	start thread with handle_usr
+        start thread with handle_usr
     '''
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # set up a listening socket
-    	s.bind(ADDR)
+        s.bind(ADDR)
         print("Waiting for a connection . . .")
-    	s.listen() # listen for a connection
+        s.listen() # listen for a connection
         while True: # for each connection
             conn, addr = s.accept() # accept it
             print(f"Connection from {addr}")
             thread = Thread(target=handle_usr, args=(conn, addr)) # init thread
             thread.start() # start thread
-            print(f"Active connections: {activeCount() - 1}")
-          
-  
-if __name__ == __main__:
+            print(f"Active connections: {active_count() - 1}")
+
+if __name__ == "__main__":
     main()
