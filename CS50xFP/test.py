be9117c2f96225e22740e88e57e01bab8e25548a
pacse <68709871+pacse@users.noreply.github.com>
2025-06-17 07:14:54 +1200

    General updates

--- CS50xFP/test.py
+++ CS50xFP/test.py
@@ -1,8 +1,7 @@
 from utils import db
 
-rows = db.execute("SELECT id, name FROM containment_class")
+rows = db.execute("SELECT * FROM audit_log")
 
 with open("output.txt", "w") as f:
     for row in rows:
-        line = 'printc(f"{row["id"]} - {row["name"]}")'
         f.write(f"printc(\"{row['id']} - {row['name']}\")\n")
