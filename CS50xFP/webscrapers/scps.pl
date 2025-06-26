994b28240e7c4e85df10b65a9b7eda0a808f5a82
pacse <68709871+pacse@users.noreply.github.com>
2025-06-26 21:02:36 +1200

    Create scps.pl
    Adding a script to filter and format some of the html for markdown. Will expand and integrate with webscraper tmw

---
+++ CS50xFP/webscrapers/scps.pl
@@ -0,0 +1,26 @@
+# looking at how we would do text processing with perl.
+# this is my first Perl script, so pardon bad form.
+# make script executable: chmod +x scps.pl
+
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Slurp input from python
+undef $/;
+my $html = <STDIN>;
+
+# === modify html ===
+# proper italics
+$html =~ s/<em>/*/gis; # replace all <em> with *, g: match all, i: case insensitive, s: contd with \n ect
+$html =~ s/<\/em>/*/gis; # replace all </em> with *
+
+# proper bolding
+$html =~ s/<strong>/"**"/gis; # replace all <strong> with **
+$html =~ s/<\/strong>/"**"/gis; # replace all </strong> with **
+
+# remove wikidot footnotes
+$html =~ s{<sup class="footnoteref">.*?</sup>}{}gis;
+
+# === print html ===
+print $html;
