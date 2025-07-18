a506c8f4e8b5ff303b2298edf9052f22f723103e
pacse <68709871+pacse@users.noreply.github.com>
2025-07-18 12:49:49 +1200

    Create basic.py
    Moved frequent and simple functions. Expanded in future

---
+++ CS50xFP/utils/basic.py
@@ -0,0 +1,28 @@
+'''
+Low level functions and vars
+'''
+from typing import TypeAlias, Any
+from os import name, system
+from datetime import datetime as dt
+
+Unknown: TypeAlias = Any
+'''temp type for incomplete type annotations'''
+
+
+def clear() -> None:
+    '''
+    Clear the screen
+    '''
+    # OS is windows
+    if name == 'nt':
+        system("cls")
+    # OS is mac or linux (or any other I guess, it's an else statement...)
+    else:
+        system("clear")
+
+def timestamp() -> str:
+  '''
+  gets the current timestamp
+  format: Day/Month/Year - Hour/min/second
+  '''
+  return dt.now().strftime("%d/%m/%Y - %H:%M:%S")
