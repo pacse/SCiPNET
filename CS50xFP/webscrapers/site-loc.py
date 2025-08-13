050f76bf6d1b33d5edfc7afa4e4f79797a5604ce
pacse <68709871+pacse@users.noreply.github.com>
2025-08-13 13:26:03 +1200

    Create site-loc.py

---
+++ CS50xFP/webscrapers/site-loc.py
@@ -0,0 +1,7 @@
+'''
+Script to find the location of a site, based on it's left & top positions on
+the large map here: https://scp-wiki.wikidot.com/secure-facilities-locations
+
+Created by me, with teaching from ChatGPT
+(chat: )
+'''
