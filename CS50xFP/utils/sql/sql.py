97485d1ceb356f398890efc91dcde36760d51260
pacse <68709871+pacse@users.noreply.github.com>
2025-09-07 20:19:14 +1200

    Moved BaseModels from sql.py to models.py

--- CS50xFP/utils/sql/sql.py
+++ CS50xFP/utils/sql/sql.py
@@ -70,7 +70,7 @@ def next_id(table_name: str) -> int:
         )
 
 
-def get_field_with_field(table_name: str, 
+def get_field_with_field(table_name: str,
                          lookup_field: str, lookup_value: str | int,
                          return_field: str
                         ): # impossible to know return type
@@ -80,10 +80,10 @@ def get_field_with_field(table_name: str,
     # validate inputs
     if not validate_table(table_name):
         raise TableNotFoundError(table_name)
-    
+
     if lookup_field == "id" and not isinstance(lookup_value, int):
         raise ValueError(f"Invalid ID: {lookup_value!r} (expected int, got {type(lookup_value).__name__})")
-    
+
     elif lookup_field == "name" and not isinstance(lookup_value, str):
         raise ValueError(f"Invalid name: {lookup_value!r} (expected str, got {type(lookup_value).__name__})")
 
@@ -93,10 +93,10 @@ def get_field_with_field(table_name: str,
     try:
         query = f"SELECT {return_field} FROM {table_name} WHERE {lookup_field} = ?"
         return db.execute(query, lookup_value)[0][return_field]
-    
+
     except IndexError:
         raise RecordNotFoundError(table_name, lookup_field, lookup_value)
-    
+
     except (RuntimeError, KeyError): # db.execute will raise RuntimeError, KeyError just in case
         raise ColumnNotFoundError(lookup_field, table_name)
 
@@ -118,7 +118,7 @@ def get_name(table: str, table_id: int) -> str:
 def get_nickname(MTF_id: int) -> str:
     '''
     Gets an MTF's nickname
-    
+
     Returns the nickname as a string
     '''
     return get_field_with_field("mtfs", "id", MTF_id, "nickname")
@@ -127,7 +127,7 @@ def get_nickname(MTF_id: int) -> str:
 def get_id(table: str, name: str) -> int:
     '''
     Gets the ID of a row in a table by its name
-    
+
     Returns the ID as an integer
     '''
     return get_field_with_field(table, "name", name, "id")
@@ -136,7 +136,7 @@ def get_id(table: str, name: str) -> int:
 def get_colour(id: int) -> int:
     '''
     Gets a ID's colour (for art.py) from a table
-    
+
     Returns DB hex code
     '''
     return get_field_with_field("colours", "id", id, "hex_code")
@@ -144,7 +144,7 @@ def get_colour(id: int) -> int:
 def get_cc_colour(id: int) -> int:
     '''
     Gets a containment class's colour (for art.py)
-    
+
     Returns a hex colour code
     '''
     # slightly different for containment classes,
@@ -164,7 +164,7 @@ def get_cc_colour(id: int) -> int:
 
     # convert to hex & return
     return int("".join(f"{n:02x}" for n in colours), 16)
-    
+
 
 # Log events in the audit log (eg. account creation, login, file access, file edit, ect.)
 def log_event(user_id: int,
@@ -192,185 +192,7 @@ def log_event(user_id: int,
 
     # execute query
     query = """
-            INSERT INTO audit_log (user_id, action, details, user_ip, timestamp, status) 
+            INSERT INTO audit_log (user_id, action, details, user_ip, timestamp, status)
             VALUES (?, ?, ?, ?, ?, ?)
             """
     return db.execute(query, user_id, action, details, user_ip, timestamp, status)
-
-''' 
-BaseModels for ease of
-access for deepwell stuff
-'''
-
-class User(BaseModel):
-    '''
-    A BaseModel to store user information
-    after getting their data from the deepwell
-    '''
-    u_id: int
-    name: str
-    password: str
-    clearance_level_id: int
-    clearance_level_name: str
-    title_id: int
-    title_name: str
-    site_id: int
-    ip: str # the ip that the user has connected from
-    override_phrase: str | None = Field(None, description="Not required for lower level personnel")
-
-# TODO: Remake
-def init_usr(info: dict[str, str | int | None]) -> User:
-    pass
-    """
-    '''
-    Creates a user BaseModel from
-    the user's deepwell info
-    '''
-
-    return User(
-        u_id = cast(int, info["id"]),
-        name = cast(str, info["name"]),
-        password = cast(str, info["password"]),
-        clearance_level_id = cast(int, info["clearance_level_id"]),
-        clearance_level_name = get_name("clearance_levels", cast(int, info["clearance_level_id"])),
-        title_id = cast(int, info["title_id"]),
-        title_name = get_name("titles", cast(int, info["title_id"])),
-        site_id = cast(int, info["site_id"]),
-        override_phrase = cast(str | None, info["override_phrase"])
-      )
-"""
-
-class SCP_Colours(BaseModel):
-    '''
-    BaseModel to store hex colour codes 
-    (as ints) for the display of an SCP
-    '''
-    class_lvl: int
-    cont_clss: int
-    disrupt_clss: int
-    rsk_clss: int
-
-# TODO: Remake
-"""def init_colours(classification_level: int, containment_class: int,
-                disruption_class: int, risk_class: int) -> SCP_Colours:
-        '''
-        Creates a SCP_COLOURS dataclass from
-        deepwell values
-        '''
-        return SCP_Colours(
-            class_lvl = get_colour(classification_level),
-            cont_clss = get_cc_colour(containment_class),
-            disrupt_clss = get_colour(disruption_class),
-            rsk_clss = get_colour(risk_class)
-        )
-"""
-
-class SCP(BaseModel):
-    '''
-    A dataclass to store a SCP's information
-    after getting its data from the deepwell
-    '''
-    scp_id: int
-    classification_level: str
-    containment_class: str
-    secondary_class: str | None = None
-    disruption_class: str
-    risk_class: str
-    site_responsible_id: int | None = None
-    assigned_task_force_name: str | None = None
-    colours: SCP_Colours
-
-# TODO: Remake
-def init_scp(info: dict[str, str | int | None]) -> SCP:
-    pass
-    """
-    # TODO!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    # handle null values properly you lazy cabrone (╯°□°）╯︵ ┻━┻
-    '''
-    Creates a scp dataclass from
-    the scp's deepwell info
-    '''
-
-    # get proper names from deepwell
-    classification_level = get_name("clearance_levels",
-                                    cast(int, info["classification_level_id"]))
-        
-    containment_class = get_name("containment_classes",
-                                 cast(int, info["containment_class_id"]))
-    
-    
-    secondary_class = get_name("secondary_classes",
-                                cast(int, info["secondary_class_id"]))
-
-    disruption_class = get_name('disruption_classes', cast(int, info['disruption_class_id']))
-    
-    risk_class = get_name("risk_classes", cast(int, info["risk_class_id"]))
-
-    if info["assigned_task_force_id"]:
-        atf_name = get_name("mtfs", cast(int, info["assigned_task_force_id"]))
-        atf_nickname = get_nickname(cast(int, info["assigned_task_force_id"]))
-        assigned_task_force_name = f"{atf_name} (*{atf_nickname}*)"
-    else:
-        assigned_task_force_name = None
-
-    
-    return SCP(
-        id=cast(int, info["id"]),
-        classification_level = classification_level,
-        containment_class = containment_class,
-        secondary_class = secondary_class,
-        disruption_class = disruption_class,
-        risk_class = risk_class,
-        site_responsible_id = cast((int | None), info["site_responsible_id"]),
-        assigned_task_force_name = assigned_task_force_name,
-        
-        colours = init_colours(
-            classification_level = cast(int, info["classification_level_id"]),
-            containment_class = cast(int, info["containment_class_id"]),
-            disruption_class = cast(int, info["disruption_class_id"]),
-            risk_class = cast(int, info["risk_class_id"]),
-        )
-    )
-"""
-
-class Site:
-    s_id: int
-    name: str
-    director: str | None = None
-
-# TODO: Remake
-def init_site(info: dict[str, str | int | None]) -> Site:
-    pass
-    """
-    # format director
-    director: str | None = f"{info['director_id']} - {info['director_name']}" if info["director_id"] is not None else "[REDACTED]"
-
-    return Site(
-        s_id = cast(int,info["site_id"]),
-        name = cast(str, info["site_name"]),
-        director = director
-    )
-"""
-
-class MTF:
-    mtf_id: int
-    name: str
-    nickname: str
-    leader_id: int | None = None
-    leader_name: str | None = None
-    site_id: int | None = None
-    active: bool
-
-# TODO: Remake
-def init_mtf(info: dict[str, str | int | None]) -> MTF:
-    pass
-    """
-    return MTF(
-        id = cast(int, info["mtf_id"]),
-        name = cast(str, info["mtf_name"]),
-        nickname = cast(str, info["mtf_nickname"]),
-        leader_id = cast(int | None, info["leader_id"]),
-        leader_name = cast(str | None, info["leader_name"]),
-        site_id = cast(int | None, info["mtf_site_id"]),
-        active = cast(bool, info["mtf_active"])
-    )"""
