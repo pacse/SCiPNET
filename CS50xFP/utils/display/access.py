3e21e9892dc4c69ad7011a486664b7a9a499100b
pacse <68709871+pacse@users.noreply.github.com>
2025-10-05 19:39:52 +1300

    Create access.py
    Holds the following file access funcs:
    - redacted
    - expunged
    - granted

---
+++ CS50xFP/utils/display/access.py
@@ -0,0 +1,63 @@
+"""
+Display functions related to file access
+
+Included in *:
+- redacted()
+- expunged()
+- granted()
+
+Excluded from *:
+- all imports
+"""
+
+from .core.boxes import basic_box_with_text
+
+def redacted(file: str, file_classification: str, usr_clearance: str) -> None:
+    """
+    prints a message saying `file` is above your clearance
+
+    art by ChatGPT
+    """
+
+    basic_box_with_text(
+        [
+         f'FILE_REF: {file} REDACTED',
+         f'CLEARANCE {file_classification} REQUIRED',
+         f'(YOU ARE CLEARANCE {usr_clearance})'
+        ],
+        ['ACCESS DENIED'],
+
+    )
+
+
+def expunged(file: str) -> None:
+    """
+    prints a message saying `file` has been expunged
+
+    art by ChatGPT
+    """
+
+    basic_box_with_text(
+        [f'FILE_REF: {file} NOT FOUND'],
+        ['DATA EXPUNGED'],
+    )
+
+
+def granted(file: str) -> None:
+    """
+    prints a message saying access has been granted to `file`
+
+    art by ChatGPT
+    """
+
+    basic_box_with_text(
+        [f'FILE_REF: {file} ACCESS GRANTED'],
+        ['ACCESS GRANTED'],
+    )
+
+
+__all__ = [
+    'redacted',
+    'expunged',
+    'granted'
+]
