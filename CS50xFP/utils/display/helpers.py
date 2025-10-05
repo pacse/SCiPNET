a22497458a020c838b8ed0d8c8bb51d40023a287
pacse <68709871+pacse@users.noreply.github.com>
2025-10-05 19:44:00 +1300

    Update helpers.py

--- CS50xFP/utils/display/helpers.py
+++ CS50xFP/utils/display/helpers.py
@@ -1,5 +1,52 @@
+"""
+Helpers for display functions
+
+Included in *:
+- printc()
+- print_lines()
+- clear()
+- timestamp()
+- f_id()
+
+Excluded from *:
+- all imports
+"""
+
+from .config import SIZE
+
 from os import name, system
 from datetime import datetime
+
+def f_id(id: int) -> str:
+    """
+    Formats a file id to be 3 digits with leading zeros
+    """
+    return f'{id:03}'
+
+# helpers
+def printc(
+           string: str,
+           end: str = '\n',
+           flush: bool = False
+          ) -> None:
+  """
+  prints {string} centered to the terminal size
+  """
+  print(string.center(SIZE), end=end, flush=flush)
+
+def print_lines(
+                lines: list[str]
+               ) -> None:
+    """
+    printc's all lines provided
+    """
+    for line in lines:
+        if line == '':
+            print()
+        else:
+            printc(line)
+
+
 def clear() -> None:
     '''
     Clear the screen
@@ -20,3 +67,11 @@ def timestamp() -> str:
   '''
   curr_dt = datetime.now()
   return curr_dt.strftime("%Y/%m/%d - %H/%M/%S")
+
+__all__ = [
+    'printc',
+    'print_lines',
+    'clear',
+    'timestamp',
+    'f_id'
+]
