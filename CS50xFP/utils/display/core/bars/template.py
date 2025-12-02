4ae3b493d9db53705c67c08a9401d47bd41f99e8
pacse <68709871+pacse@users.noreply.github.com>
2025-12-02 16:21:20 +1300

    Update template.py
    Added is_acs config bool, because ACS bars have different t & m separator lines

--- CS50xFP/utils/display/core/bars/template.py
+++ CS50xFP/utils/display/core/bars/template.py
@@ -13,17 +13,20 @@ class BarTemplate:
 
     Args:
         console (Console | None): Console to print to
-        has_center_column (bool): Whether the bar has a center column
+        has_center_column (bool): does the bar has a center column
         width (int): Total width of the bar
+        is_acs (bool): is this the acs bar
     """
 
     def __init__(self,
                  console: Console | None = None,
                  has_center_column: bool = False,
                  width: int = MIN_TERM_WIDTH,
+                 is_acs: bool = False
                 ) -> None:
 
         # === Init basic self vars ===
+        self.is_acs = is_acs                                    # is this the acs bar
         self.width = width                                      # total width of bar
         self.console = console if console else Console()        # Console to print to
         self.left_align = ' ' * ((SIZE - MIN_TERM_WIDTH) // 2)  # spaces to left align
@@ -51,6 +54,10 @@ class BarTemplate:
                         'm':f'╠{rept}╬{rept}╬{rept}╣',
                         'b':f'╚{rept}╩{rept}╩{rept}╝'
                        }
+
+            if not is_acs: # ACS is slightly different
+                self.sep['t']  = f'╔{rept}═{rept}═{rept}╗'
+                self.sep['m'] = f'╠{rept}╦{rept}╦{rept}╣'
         else:
             self.sides = ['l', 'r']
             self.sep = {
@@ -59,11 +66,16 @@ class BarTemplate:
                         'b':f'╚{rept}╝╚{rept}╝'
                        }
 
+            if not is_acs: # ACS is slightly different
+                self.sep['t']  = f'╔{rept}══{rept}╗'
+                self.sep['m'] = f'╠{rept}╗╔{rept}╣'
+
+
         # type annotate self.sides
         self.sides: list[Literal['l', 'r', 'c']]
 
 
-
+    # === Helper Method ===
     def _render_sep(self,
                     pos: Literal['t', 'm', 'b']
                    ) -> None:
@@ -75,12 +87,8 @@ class BarTemplate:
         """
         try:
             self.console.print(f'{self.left_align}{self.sep[pos]}')
-
         except KeyError:
-            raise FieldError(
-                'pos', pos,
-                "'t', 'm', or 'b'"
-            )
+            raise FieldError('pos', pos, "'t', 'm', or 'b'")
 
 
     # === Main Method ===
